e14d66614485078732459aca2b56394313a54061
kleigh23 <rob22003@byui.edu>
2022-12-09 11:49:10 -0700

    cleaned up

--- final_project/scripts/main.js
+++ final_project/scripts/main.js
@@ -8,50 +8,27 @@ const picWeapon = document.querySelector("#imgWeapon");
 
 
 const elements = "http://api.genshin.dev/elements"
-const characters = "http://api.genshin.dev/characters"
+const characters = "https://api.genshin.dev/characters"
 const weapons = "http://api.genshin.dev/weapons"
 
 fetch(characters)
     .then((response) => response.json())
     .then((data) => {
-        // console.log(data);
         data.forEach(characterButtons);
-        //addEvent();
     })
 
 
 
 function characterButtons(data) {
     let card = document.createElement('section');
-    //let button = document.createElement('button');
-    //let link = document.createElement('a');
-    //let img = document.createElement('img');
-    //link.href = "character_view.html";
     const icon = `https://api.genshin.dev/characters/${data}/icon.png`;
-    // button.setAttribute("id", data);
 
-    console.log(typeof data);
+
     let Name = data.toUpperCase();
     let characterId = data.toLowerCase();
     let cardinnerHTML = `<button id='${data}' class="characters" onclick='setLocalStorage(${characterId})'>${Name}</button>`;
     cardinnerHTML += `<img src='${icon}' alt='${data}' class='images'/>`
     card.innerHTML = cardinnerHTML;
-
-    // let ID = button.id;
-    // console.log(ID);
-    // button.setAttribute("onclick", setLocalStorage(ID));
-    // img.setAttribute('src', icon);
-    // img.setAttribute('alt', data);
-    // img.setAttribute('id', 'images')
-    
-    //button.textContent = (Name);
-    
-    //link.appendChild(button);
-
-    //button.classList.add("characters");
-
-    //card.appendChild(img);
-    //card.appendChild(link);
 
     document.querySelector('div.cards').appendChild(card);
 
